a70c4287a768aba451d2ee6ba9e6e8edef3b196d
ksemenza <ksemenza13@gmail.com>
2020-12-08 21:25:57 -0500

    post details reducer

--- src/state-reducers/post-reducer.jsx
+++ src/state-reducers/post-reducer.jsx
@@ -3,10 +3,22 @@ import {LOCAL_URL, USER_URL, POST_URL, DETAILS_URL} from '../constants/endpoints
 
 export const POST_REQ = "POST_REQ";
 export const POST_SUC = "POST_SUC";
-export const POST_FAIL = "POST_FAIL";
+export const POST_FAIL = "POST_FAIL"; n 
 
 //Get /api/post
 export const getPosts = (posts, history) => dispatch => {
+    dispatch({type:POST_REQ});
+    axios.get(`${LOCAL_URL}${POST_URL}`, posts)
+    .then(res => {
+        dispatch({type: POST_SUC, payload:res.data})
+        history.push('/post');
+    })
+    .catch(err => {
+        dispatch({type:POST_FAIL, payload:err.res});
+    });
+}
+//Get /api/post
+export const getPostDetails = (posts, history) => dispatch => {
     dispatch({type:POST_REQ});
     axios.get(`${LOCAL_URL}${POST_URL}`, posts)
     .then(res => {
