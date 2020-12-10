69f59fc1765d09c182656542b94571ed6f89d876
ksemenza <ksemenza13@gmail.com>
2020-12-09 20:22:42 -0500

    single view state

--- src/components/PostSingle.jsx
+++ src/components/PostSingle.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {axiosAuth} from '../api/axiosAuth'
 
 const PostSingle = (props) => {
 
-console.log(props.location.pathname)
+    const [post, setPost] = useState('')
+    console.log(props.location.pathname)
+    
+     useEffect(() => {
+                axiosAuth()
+        .get(`${props.location.pathname}`)
+  
+                    .then((res) => {
+                        console.log(res.data)
+                        setPost(res.data)
+        })
+        .catch((err) => {
+            console.log(`Get User Post Error`, err)
+        })
+     }, [])
+    
+    console.log(post.title)
 
     return (
         <div className='post-view-cta'>
             <h2>Post Single</h2>
+            {post.title}
         
         </div>
     )
