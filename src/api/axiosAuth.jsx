97da87e57469db004ef20d81d5910e558707a871
ksemenza <ksemenza13@gmail.com>
2020-12-11 00:28:39 -0500

    deployed main

--- src/api/axiosAuth.jsx
+++ src/api/axiosAuth.jsx
@@ -7,7 +7,7 @@ const DB_URL = process.env.BE_URL || LOCAL_URL
 export const axiosAuth = () => {
     const token = localStorage.getItem('token');
     return axios.create({
-        baseURL: `${DB_URL}`,
+        baseURL: `${DEPLOYED_URL}`,
         headers: {
             Authorization: token
         }
