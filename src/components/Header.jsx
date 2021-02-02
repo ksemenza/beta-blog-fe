3051fdc67b7257a57dd5ebc295ab0b4bbe7bc871
ksemenza <ksemenza13@gmail.com>
2021-02-02 16:07:32 -0500

    updated ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,32 +11,29 @@ const Header = props => {
 
 
     return (
-        <div className='header-cta'>
-          
-            <h2 className='header-title'>Beta Blog What's On Your Nog</h2>
-             <div className='nav-logout-wrap'>
-                  <Link to='/'>
-                <button
-                className='header-btn-logout'
-                onClick={props.logoutUser}>
-                    Log Out
-                </button>
+      <div className="header-cta">
+        <div className="nav-logout-wrap">
+          <h2 className="header-title">Beta Blog What's On Your Nog</h2>
+          <Link to="/">
+            <button className="header-btn-logout" onClick={props.logoutUser}>
+              Log Out
+            </button>
+                </Link>
+                </div>
+          <nav className="nav-link-wrap">
+
+            <Link className="header-link-nav" to={`/auth/${USER_ID}`}>
+              Profile
             </Link>
-                <nav className='nav-link-wrap'>
-                    <Link className='header-link-nav' to='/homepage'>Home</Link>
-                    <Link className='header-link-nav' to={`/auth/${USER_ID}`}>Profile</Link>
-                    {/* <Link className='header-link-nav' to={`/auth/${USER_ID}/details`}>Details</Link> */}
-                   <a href="/post" className='header-link-nav' >My Posts</a>
-                    {/* <Link className='header-link-nav' to='/comment'>My Comments</Link> */}
-                    {/* <Link className='header-link-nav'  to='/notifications'>Notification</Link> */}
-                    </nav>
-            
-          
-            </div>
-            </div>
-        
-    
-    )
+            {/* <Link className='header-link-nav' to={`/auth/${USER_ID}/details`}>Details</Link> */}
+            <a href="/post" className="header-link-nav">
+              My Posts
+            </a>
+            {/* <Link className='header-link-nav' to='/comment'>My Comments</Link> */}
+            {/* <Link className='header-link-nav'  to='/notifications'>Notification</Link> */}
+          </nav>
+      </div>
+    );
 }
 
 const mapStateToProps = state => {
